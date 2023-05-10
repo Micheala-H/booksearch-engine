341a33d63a2491ba763909405aaa5aeeb995e553
Micheala-H <mholthus17@gmail.com>
2023-05-09 21:07:43 -0500

    edited queries and added the graphql and apollo

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,77 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    user: async (parent, { username }) => {
+      return User.findOne({ username });
+    },
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id });
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+
+  Mutation: {
+    createUser: async (parent, { username, email, password, savedBooks }) => {
+      const user = await User.create({ username, email, password, savedBooks });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('No user found with this email address');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+
+      return { token, user };
+    },
+    saveBook: async (parent, { user, body }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: user._Id },
+          {
+            $addToSet: {
+              savedBooks:  body ,
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+    deleteBook: async (parent, { user }, context) => {
+      if (context.user) {
+        return User.findOneAndUpdate(
+          { _id: user._Id },
+          {
+            $pull: {
+              savedBooks: {
+                bookId: context._id, 
+              },
+            },
+          },
+          { new: true }
+        );
+      }
+      throw new AuthenticationError('You need to be logged in!');
+    },
+  },
+};
+
+module.exports = resolvers;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,40 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+    savedBooks: [Book]!
+  }
+
+  type Book {
+    _id: ID
+    authors: String
+    description: String
+    bookId: String
+    image:String
+    link: String
+    title: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    user(username: String!): User
+    me: User
+  }
+
+  type Mutation {
+    createUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+    savedBook( user: String!, body: String!): Book
+    deleteBook( user: String!): Book
+  }
+`;
+
+module.exports = typeDefs;
